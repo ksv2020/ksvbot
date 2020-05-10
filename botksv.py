927ce3b831c399edfa51994ba2096c1f6d0418f0
ksv2020 <61200452+ksv2020@users.noreply.github.com>
2020-05-10 23:42:19 +0300

    Add files via upload

--- botksv.py
+++ botksv.py
@@ -12,8 +12,8 @@ bot = telebot.TeleBot(token)
 
 # Задаю переменную, чтобы проверять произошел ли уже парсинг или нет
 parsed = False
-# Задаю переменную со списком стран, которые поддерживает мой бот
-supported = ['France', 'China', 'Italy', 'Russia']
+# Задаю переменную со списком валют, которые поддерживает мой бот
+supported = ['USD', 'EUR']
 
 # Команды
 
@@ -24,17 +24,14 @@ def show_start(message):
     # метод класса send_message берет два аргумента - кому отправляем сообщение и сообщение, которое отправляем.
     # объект message - это сообщение от пользователя, в этом классе есть атрибут с метадатой
     # из которого мы достаем id пользователя, который его отправил и отвечаем этому пользователю
-    bot.send_message(message.from_user.id, "Добрый день. Я умею работать с сайтом https://www.worldometers.info/coronavirus/.\
-Если вы введете название страны на английском языке я выведу вам статистику с 15 февраля: сколько было \
-зафиксировано новых случаев и смертей за каждый из день. После того, как я соберу данные, я смогу вывести следующую информацию: \
-ввывести среднее количество случаев или смертей (по запросу) за это время, вывести количество новых случаев или смертей за \
-любую дату, вывести количество дней, в которые было зафиксировано больше случаев или смертей, чем в предыдущие. \
-Чтобы посмотреть все команды нажмите /help. Для начала парсинга нажмите /parse. Для просмотра доступных стран для парсинга нажмите /parse_help.")
+    bot.send_message(message.from_user.id, "Добрый день. Я умею работать с сайтом https://www.cbr.ru/currency_base/\
+Если вы введете валюту и дату я сообщу ее курс к рублю\
+Чтобы посмотреть все команды нажмите /help. Для начала парсинга нажмите /parse. Для просмотра доступных валют для парсинга нажмите /parse_help.")
 
 # все то же, что выше, только реагируем на команду /help
 @bot.message_handler(commands=['help'])
 def show_help(message):
-    bot.send_message(message.from_user.id,"/parse - ввести страну и запустить парсинг\n/parse_help - вывести список поддерживаемых стран\
+    bot.send_message(message.from_user.id,"/parse - ввести валюту и запустить парсинг\n/parse_help - вывести список поддерживаемых валют\
     \n/file - получить файл с данными\n/median - посчитать медиану для выбранной колонки\
     \n/mean - посчитать среднее для выбранной колонки\n/date - получить информацию по конкретному дню")
 
@@ -44,141 +41,48 @@ def show_help(message):
 def parse(message):
     global parsed
     parsed = False
-    bot.send_message(message.from_user.id, "Введите название страны на английском языке: ") # запрашиваем название страны
+    bot.send_message(message.from_user.id, "Введите код валюты на английском языке и дату: ") # запрашиваем название валюты
     
-# реагируем на команду /parse_help. Выводим страны из списка, для которых можем собрать информацию    
+# реагируем на команду /parse_help. Выводим валюты из списка, для которых можем собрать информацию    
 @bot.message_handler(commands=['parse_help'])
 def show_parse_help(message):
-    bot.send_message(message.from_user.id, f"Пока я могу собрать информацию только для этих стран:\n {' '.join(supported)}")
+    bot.send_message(message.from_user.id, f"Пока я могу собрать информацию только для этих валют:\n {' '.join(supported)}")
     
 # реагируем на команду /file, если parsed = True, т.е. парсинг завершен, то будем высылать пользователю файл с собранной информацией    
-@bot.message_handler(commands=['file'])
-def get_file(message):
-    global parsed
-    if parsed: # проверяем, что парсинг завершился
-        fh = open('data.csv', 'rb') # наш файл, который после парсинга сохраняется локально или на сервере. Открываем его.
-        bot.send_document(message.from_user.id, fh) # отправляем файл, с которым работаем, пользователю
-        fh.close() # закрываем файл
-    else:
-        # если информация не собрана, то скажем об этом пользователю и подскажем, как запустить процесс
-        bot.send_message(message.from_user.id, "Парсинг не выполнен. Нажмите /parse чтобы это сделать") 
-
-# реагируем на команду /median - возвращаем медианное значение смертей или новых случаев        
-@bot.message_handler(commands=['median'])
-def get_median(message):
-    global parsed 
-    if parsed: # проверяем, что парсинг произошел
-        col = message.text.split() # парсим сообщение от пользователя, он может ввести команду и выбрать колонку - deaths или cases
-        if len(col) == 2: # проверяем, было ли в команде название колонки (split() тогда вернет список из двух элементов)
-            if col[1] == 'cases': # если пользователь ввел cases, то выбираем нужную колонку
-                col = 'number of daily cases'
-            else: # то же самое для deaths
-                col = 'number of daily deaths'
-        else: # если пользователь ввел только команду /median, то выбираем колонку по умолчанию (cases)
-            col = 'number of daily cases'
-# здесь мы не сделали обработку ошибок (что если пользователь ввел три слова, ввел неправильное название колонки и т.д.)
-# можно такую обработку добавить
-        data = pd.read_csv('data.csv', delimiter = ',') # считываем нашу таблицу с помощью pandas
-        med = data[col].median() # методом колонки таблицы pandas находим медиану для выбранной колонки
-        bot.send_message(message.from_user.id, "Медиана для колонки " + col + " = " + str(med)) # отправляем сообщение с найденой медианой
-    else:
-        # обрабатываем случай, если пользователь вызвал команду /median до того, как мы собрали информацию
-        bot.send_message(message.from_user.id, "Парсинг не выполнен. Нажмите /parse чтобы это сделать")
-
-# все то же самое, что выше только для арифметического среднего        
-@bot.message_handler(commands=['mean'])
-def get_mean(message):
-    global parsed
-    if parsed:
-        col = message.text.split()
-        if len(col) == 2:
-            if col[1] == 'cases':
-                col = 'number of daily cases'
-            else:
-                col = 'number of daily deaths'
-        else:
-            col = 'number of daily cases'
-            
-        data = pd.read_csv('data.csv', delimiter = ',')
-        mea = data[col].mean()
-        bot.send_message(message.from_user.id, "Среднее для колонки " + col + " = " + str(mea))
-    else:
-        bot.send_message(message.from_user.id, "Парсинг не выполнен. Нажмите /parse чтобы это сделать")
-
-# реагируем на команду /date - выводим информацию о новых случаях в определенный день, который вводит пользователь 
-@bot.message_handler(commands=['date'])
-def get_date(message):
-    global parsed
-    if parsed: # проверяем, что парсинг состоялся
-        col = message.text.split() # мы ожидаем сообщение в формате '/date Feb 02', разбиваем по пробелам
-        if len(col) != 3: # топорно обрабатываем ошибку, если в разбитом сообщение не три элемента (команда, месяц и дата)
-            # тут, конечно же, можно сделать более тонкую обработку случаев с помощью регулярных выражений
-            bot.send_message(message.from_user.id, "Дата не указана.")
-        mon = col[1] # сохраняем месяц в переменную
-        day = col[2] # сохраняем день
-        try:
-            data = pd.read_csv('data.csv', delimiter = ',') # считываем таблицу с помощью pandas
-            # выводим сообщение с информацией
-            bot.send_message(message.from_user.id, f'{mon + " " + day} было зарегестрировано {data[data.date == mon + " " + day]["number of daily cases"].values[0]} случаев')
-        except Exception:
-            # выводим информацию об ошибке в дате
-            bot.send_message(message.from_user.id, "Ошибка в дате или дата не доступна, попробуйте еще раз.")
-    else:
-        bot.send_message(message.from_user.id, "Парсинг не выполнен. Нажмите /parse чтобы это сделать")
+# реагируем на команду /date - выводим информацию о курсе валюты в определенный день, который вводит пользователь 
     
 # Обабатываем все остальные сообщения от пользователя, которые не являются командами, прописанными выше
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     global parsed
     if not parsed: # проверяем, что парсинг не произошел
-        if message.text in supported: # проверяем, что введенное сообщение является названием страны, для которой можем сделать парсинг
+        if message.text in supported: # проверяем, что введенное сообщение является наименованием валюты, для которой можем сделать парсинг
             try: # пытаемся выполнить парсинг
                 bot.send_message(message.from_user.id, "Начинаю парсинг. Подождите...") # сообщаем пользователю, что начали работу
-                
-                url = f'https://www.worldometers.info/coronavirus/country/{message.text.lower()}' # переходим по ссылке, для заданной страны
+                if message.split()[0] == 'USD': cur = 'R01235'
+                elif message.split()[0] == 'EUR': cur = 'R01239'
+                date_cur = message.split()[1]
+
+                url = f'http://www.cbr.ru/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.mode=1&UniDbQuery.date_req1=&UniDbQuery.date_req2=&UniDbQuery.VAL_NM_RQ={cur.lower()}&UniDbQuery.From={date_cur}&UniDbQuery.To={date_cur}' # переходим по ссылке, для заданного
 
                 html = requests.get(url).text
                 soup = bs4.BeautifulSoup(html, 'lxml')
-
-                dates_cases = []
-                cases = []
-                
+                soup.prettify()
+                soup.find_all('table')
+                soup.find_all('td')
+                avr = []
+                sum = 0
+                for idx in soup.find_all('td'):
+                    curs = re.findall(r'\d\d\,\d{4}', str(idx)[4:-5])
+                    if curs != []: 
+                        curs = ",".join(curs)
+                        avr.append(float(curs.replace(",",".")))
+                        sum += avr[-1]
+                print('average:', sum/len(avr))               
                 # ниже привычный нам парсинг, единственное, используем str(graph) вместо graph.text,
-                # почему-то heroku воспринимает атрибут объекта beautiful soup пустым
-                for graph in soup.find_all('script', {'type': "text/javascript"}): # достаем информацию о новых случаях из графика
-                    if 'Daily New Cases' in str(graph): 
-                        dates_cases = re.findall(r'categories: \[([\w\s",]+)', str(graph))[0]
-                        cases = re.findall(r'data: \[([\w,]+)', str(graph))[0]
-
-                dates_deaths = []
-                deaths = []
-                # то же самое для новых зарегистрированных смертей
-                for graph in soup.find_all('script', {'type': "text/javascript"}):
-                    if 'Daily Deaths' in str(graph):
-                        dates_deaths = re.findall(r'categories: \[([\w\s",]+)', str(graph))[0]
-                        deaths = re.findall(r'data: \[([\w,]+)', str(graph))[0]
-                 
-                # избавляемся от "" в датах и делаем список
-                dates = [date.strip('"') for date in dates_cases.split(',')]
-                # заменяем возможные вхождения null на '0'
-                cases = cases.replace('null', '0')
-                deaths = deaths.replace('null', '0')
-                cases = [int(x) for x in cases.split(',')] # генерируем списки из целых значений
-                deaths = [int(x) for x in deaths.split(',')]
-
-                with open('data.csv', 'w') as fh: # открываем файл, чтобы сохранить в него собранную информацию
-                    fh.write('date,number of daily cases,number of daily deaths\n') # записываем название колонок
-                    for i in range(len(dates)):
-                        fh.write(f'{dates[i]},{cases[i]},{deaths[i]}\n') # записываем строки с данными для каждого ряда
-                        
+   
                 parsed = True # меняем метку parsed, если парсинг успешно завершилася
                 bot.send_message(message.from_user.id, "Парсинг успешно закончен. Выберите следующую команду:") # сообщаем об этом пользователю
-                # рассказываем пользователю, что умеем делать с собранными данными
-                bot.send_message(message.from_user.id, f'''/file - Получить файл с данными\ 
-                \n/median - Посчитать медиану. После команды через пробел напишите название колонки, для которой нужно найти медиану (cases или deaths)\
-                \n/mean - Посчитать среднее. После команды через пробел напишите название колонки, для которой нужно найти среднее (cases или deaths)\
-                \n/date - Получить информацию по конкретному дню. После команды через пробел месяц и день в формате Feb 15. Даты меньше 10 с ведушим нулем - 01,02\
-                \nДля {message.text} доступны данные в интервале {dates[0]} - {dates[-1]}''')
 
             except Exception:
                 # обрабатываем случай, что парсинг почему-то не завершился
