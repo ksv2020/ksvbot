8025be8a84b4bf610ab6c8ae14d5617c9fc08ecf
ksv2020 <61200452+ksv2020@users.noreply.github.com>
2020-05-11 23:01:08 +0300

    Add files via upload

--- botksv.py
+++ botksv.py
@@ -137,11 +137,11 @@ def get_text_messages(message):
                     for i in range(len(dates_curs)):
                         fh.write(f'{dates_curs[i]},{curs[i]}\n') # записываем строки с данными для каждого ряда
 
-                    parsed = True # меняем метку parsed, если парсинг успешно завершилася
-                    bot.send_message(message.from_user.id, "Парсинг успешно закончен. Выберите следующую команду:") # сообщаем об этом пользователю
-                    bot.send_message(message.from_user.id, f'''/file - Получить файл с данными\ 
-                    \n/mean - Посчитать среднее. После команды через пробел напишите месяц и год в формате mm и yyy: например, 03 2020)\
-                    \nДля {message.text} доступны данные в интервале {dates[0]} - {dates[-1]}''')
+                parsed = True # меняем метку parsed, если парсинг успешно завершилася
+                bot.send_message(message.from_user.id, "Парсинг успешно закончен. Выберите следующую команду:") # сообщаем об этом пользователю
+                bot.send_message(message.from_user.id, f'''/file - Получить файл с данными\ 
+                \n/mean - Посчитать среднее. После команды через пробел напишите месяц и год в формате mm и yyy: например, 03 2020)\
+                \nДля {message.text} доступны данные в интервале {dates[0]} - {dates[-1]}''')
 
             except Exception:
                 # обрабатываем случай, что парсинг почему-то не завершился
